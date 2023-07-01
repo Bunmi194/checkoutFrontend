064133de5a3e2a12cbd41b2270fdab7be371a001
Bunmi Oladipupo <bunmidavids194@gmail.com>
2023-07-01 10:05:02 +0100

    fix bugs and conflict

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from "react-router-dom";
 import "./main.css";
 import Header from "./Header";
 import Statistics from "./Statistics";
